e5ff4dbd68a0952f2c7e26898e350d7cf5bb8f13
brightDeanz <it21712@hua.gr>
2023-02-21 19:19:04 +0200

    fixed logo div animation

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-document.getElementById("logo-header").textContent = strings.siteLogo;
-const tabs = document.getElementsByClassName("tab");
+const logo = document.getElementById("logodiv");
+const logoDesc = document.getElementById("logo-desc");
+const logoDescHeader = logoDesc.querySelector("#desc");
+logoDescHeader.textContent = strings.siteDescString;
 
-const homeTabText = tabs[0].querySelector("#home-tab");
-const aboutTabText = tabs[1].querySelector("#about-tab");
-const contactTabText = tabs[2].querySelector("#contact-tab");
-
-homeTabText.textContent = strings.homeString;
-aboutTabText.textContent = strings.aboutString;
-contactTabText.textContent = strings.contactString;
-
-let activeTab = 0;
-tabs[activeTab].classList.add("active");
-const handleTabClick = (i) => {
-    activeTab = i;
+for (let i = 0; i < logo.children.length; i++) {
+    logo.children[i].classList.add("swipeIn");
 }
+document.addEventListener('DOMContentLoaded', () => {
 
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', () => {
-        if (activeTab === i) return;
-        tabs[activeTab].classList.remove("active");
-        activeTab = i;
-        tabs[activeTab].classList.add("active");
-    })
-}
+
+
+});
